6f460d7f25fd15b8ee811259cbaf33f95053b063
allanlalangan <allanlalangan@gmail.com>
2023-01-14 20:27:12 -0800

    build: import google fonts, update tailwind config and global css

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,13 @@
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				body: ['"DM Sans"', 'serif'],
+				heading: ['"Jura"', 'serif'],
+				display: ['"Libre Baskerville"', 'serif'],
+			},
+		},
 	},
 	plugins: [],
-}
+};
